12b1d9b657aec7b4cdd2484e33ed0fc1d5453c56
bacancy-urmit <urmit.prajapati@bacancytechnology.com>
2020-03-25 20:15:38 +0530

    removed permit strong params method for user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u|
-     u.permit(:username, :firstname, :lastname, :contact, :country, :zipcode, :email, :password) }
-
-    devise_parameter_sanitizer.permit(:account_update) { |u| 
-      u.permit(:username, :firstname, :lastname, :contact, :country, :zipcode, 
-               :email, :password, :current_password) }
+  def after_sign_in_path_for(_resource)
+    if current_user.nil?
+      new_user_registration_path
+    else
+      if current_user.has_role? :super_admin
+        super_admins_path
+      elsif current_user.has_role? :company_admin
+        company_admin_index_path
+      elsif current_user.has_role? :servicecenter_admin
+        service_center_admin_pending_appointments_path
+      else
+        root_path
+      end
+    end
   end
+
 end
