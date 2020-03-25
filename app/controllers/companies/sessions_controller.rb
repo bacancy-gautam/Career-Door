b9711dbd20600f3043bd546bf6be353e633703ae
vatsalmewada <vatsaljmewada@gmail.com>
2020-03-25 17:36:57 +0530

    company side created, notification alert added

---
+++ app/controllers/companies/sessions_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Companies::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+  before_action :redirect_me
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  protected
+
+  def redirect_me
+    if company_signed_in?
+      companies_path
+    end
+  end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
