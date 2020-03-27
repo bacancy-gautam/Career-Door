03981fdc8446729478c734dc97faa767724267fb
Bacancy-ShubhamSingh <shubhbitvit95@gmail.com>
2020-03-27 21:02:55 +0530

    company review CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: "home#index"
+
+  root to: 'home#index'
   devise_for :companies, controllers: { 
     registrations: 'companies/registrations' }
-  resources :companies, only: [:index]
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  resources :companies, only: %i[index show] do
+    resources :company_reviews
+  end
+
   resources :technologies
   resources :super_admins
   resources :resumes
