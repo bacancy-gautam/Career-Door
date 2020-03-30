42fd989f566d65808e309ed3f5d3a0fb86839d91
Bacancy-ShubhamSingh <shubhbitvit95@gmail.com>
2020-03-30 12:17:29 +0530

    fixed merge conflict

--- app/controllers/company_reviews_controller.rb
+++ app/controllers/company_reviews_controller.rb
@@ -8,11 +8,9 @@ class CompanyReviewsController < ApplicationController
   before_action :allowed_parameters, only: %i[create update]
 
   def create
-    @company_review = CompanyReview.new(company_review_params)
-    @company_review.user_id = current_user.id
-    @company_review.company_id = @company.id
+    @company_review = @company.company_reviews.new(allowed_parameters.merge(user_id: current_user.id))
     flash.alert = if @company_review.save
-                    redirect_to edit_company_company_review_path(@company.id, @company_review.id)
+                    redirect_to company_path(@company)
                     'Review saved successfully'
                   else
                     render :new
@@ -24,9 +22,11 @@ class CompanyReviewsController < ApplicationController
     @company_review = @company.company_reviews.new    
   end
 
+  def show; end
+
   def update
-    flah.alert =  if @company_review.update(company_review_params)
-                    redirect_to edit_company_company_review_path(@company_review.id)
+    flash.alert = if @company_review.update(allowed_parameters)
+                    redirect_to company_path(@company)
                     'Review updated successfully'
                   else
                     render :edit
@@ -47,18 +47,22 @@ class CompanyReviewsController < ApplicationController
   private
 
   def show_all_company_reviews
-    @company_review = company_reviews.where(user_id: current_user.id)
+    @company_reviews = @company.company_reviews.where(user_id: current_user.id)
   end
 
   def set_company
     @company = Company.find(params[:company_id])
   end
 
+  def initialize_object
+    @company_review = @company.company_reviews.new
+  end
+
   def find_company_reviews
     @company_review = CompanyReview.find(params[:id])
   end
 
-  def company_review_params
+  def allowed_parameters
     params.require(:company_review).permit(:company_review, :review_rating)
   end
 end
