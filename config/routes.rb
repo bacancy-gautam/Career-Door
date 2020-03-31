cb533e3e47c9141cae3441f306da8526dbf7727d
bacancy-urmit <urmit.prajapati@bacancytechnology.com>
2020-03-31 11:45:10 +0530

    routes for mailer confirmations are added

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@
 Rails.application.routes.draw do
   root to: "home#index"
   devise_for :companies, controllers: { 
-    registrations: 'companies/registrations' }
+    registrations: 'companies/registrations',
+    sessions: 'companies/sessions',
+    confirmations: 'companies/confirmations' }
   resources :companies, only: [:index]
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    confirmations: 'users/confirmations'
   }
   resources :technologies
   resources :super_admins
