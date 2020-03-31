ad586c3233cc8076b43e4e4b5905ca15b927206a
bacancy-urmit <urmit.prajapati@bacancytechnology.com>
2020-03-31 11:25:14 +0530

    changed show mailer errors from false to true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,18 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    # EDITOR="vi" rails credentials:edit
+    user_name: 'service.eagle12@gmail.com',
+    password: 'serviceeagle123@',
+    domain: 'gmail.com',
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  } 
 
   config.action_mailer.perform_caching = false
 
