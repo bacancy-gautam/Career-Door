68d26cec6757d21fafc10fbaf9abf04439b3b2f9
bacancy-urmit <urmit.prajapati@bacancytechnology.com>
2020-04-02 14:30:41 +0530

    skiping validaitons for sign with google and git hub

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,29 +2,43 @@
 
 class User < ApplicationRecord
   rolify
-  
-  validates :firstname, presence: true, length: { maximum: 10},format: { multiline: true, with: /^[a-zA-Z]+$/, message: 'only alphabetic characters are allowed.'}
-  validates :lastname, presence: true, length: { maximum: 10}, format: { multiline: true, with: /^[a-zA-Z]+$/, message: 'only alphabetic characters are allowed.'}
-  validates :username, presence: true, length: { maximum: 8}, format: { multiline: true, with: /\w/, message: 'should not contain any two special character together or do not start with digit' }
+
+  attr_accessor :login_social
+  validates_presence_of :firstname, :lastname, :username, :contact, :experience, :password, unless: :login_social
+  validates :firstname, length: { maximum: 10 }, format: { multiline: true, with: /^[a-zA-Z]+$/, message: 'only alphabetic characters are allowed.' }, unless: :login_social
+  validates :lastname, length: { maximum: 10 }, format: { multiline: true, with: /^[a-zA-Z]+$/, message: 'only alphabetic characters are allowed.' }, unless: :login_social
+  validates :username, length: { maximum: 8 }, format: { multiline: true, with: /\w/, message: 'should not contain any two special character together or do not start with digit' }, unless: :login_social
   validates :email, presence: true, uniqueness: true, format: { with: /[a-z]+[\.]?[a-z]+[0-9]*\@[a-z]+.com/, message: 'Email should be unique or valid!' }
-  validates :contact, presence: true, uniqueness: true, format: { with: /\+[0-9]{2}\-[0-9]{3}[\s]*[0-9]{3}[\s]*[0-9]{4}/, message: 'contact has already been taken or format is invalid' }
-  validates :experience, presence: true
-  validates :password, presence: true, confirmation: true, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$\z/, message: 'Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character' }
-  validates :password_confirmation, presence: true
+  validates :contact, uniqueness: true, format: { with: /\+[0-9]{2}\-[0-9]{3}[\s]*[0-9]{3}[\s]*[0-9]{4}/, message: 'contact has already been taken or format is invalid' }, unless: :login_social
+  validates :password, presence: true, confirmation: true, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$\z/, message: 'Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character' }, unless: :login_social
 
   has_many :resumes, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :confirmable,:recoverable, :rememberable,
-         :validatable
+         :confirmable, :recoverable, :rememberable,
+         :validatable, :omniauthable, omniauth_providers: %i[github google_oauth2]
   has_many :interested_people, dependent: :destroy
   has_many :opening_jobs, through: :interested_people
   has_many :companies, dependent: :destroy
-  has_many :company_reviews, dependent: :destroy         
+  has_many :company_reviews, dependent: :destroy
   after_create :assign_default_role
 
   def assign_default_role
     add_role(:user) if roles.blank?
+  end
+
+  def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+      user.provider = provider_data.provider
+      user.uid = provider_data.uid
+      user.email = provider_data.info.email
+      user.firstname = provider_data.info.first_name
+      user.lastname = provider_data.info.last_name
+      user.password = Devise.friendly_token[0, 20]
+      user.skip_confirmation!
+      user.login_social = true
+      binding.pry
+    end
   end
 end
