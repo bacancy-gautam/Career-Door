2dbf746e34f059593e5fe18fdd359207bcf0a1fb
vatsalmewada <vatsaljmewada@gmail.com>
2020-04-02 16:53:36 +0530

    Theme integrated and some other changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,10 @@ require("@rails/activestorage").start()
 require("channels")
 require("packs/user_index_page.js")
 require("packs/jquery.raty")
+require("packs/main.js")
+require("packs/moment.js")
+require("packs/theme/*")
+// require("packs/main.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
