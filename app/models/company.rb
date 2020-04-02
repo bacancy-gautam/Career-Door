259f6f14ad8fc4c650404eb75c7b21344dc498fd
bacancy-urmit <urmit.prajapati@bacancytechnology.com>
2020-04-02 14:24:37 +0530

    indentation modified

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,13 +3,13 @@
 class Company < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :website, presence: true, format: { with: %r{(http://www\.|https://www\.|http://|https://)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?}, message: 'invalid url' }
+  validates :website, presence: true, format: { multiline: true, with: %r{^(http://www\.|https://www\.|http://|https://)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$}, message: 'invalid url' }
   validates :address, presence: true
   validates :whichtype, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /[a-z]+[\.]?[a-z]+[0-9]*\@[a-z]+.com/, message: 'Email should be unique or valid!' }
   validates :mobile, presence: true, uniqueness: true, format: { with: /\+[0-9]{2}\-[0-9]{3}[\s]*[0-9]{3}[\s]*[0-9]{4}/, message: 'contact has already been taken or format is invalid' }
-  validates :password, presence: true, confirmation: true, if: lambda{ new_record? || !password.nil?}, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$\z/, message: 'Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character' }
-  
+  validates :password, presence: true, confirmation: true, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$\z/, message: 'Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character' }, if: -> { new_record? || !password.nil? }  # validates :password_confirmation, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :opening_jobs, dependent: :destroy
