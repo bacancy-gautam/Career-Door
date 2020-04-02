a92745823422b1b2ada257b8f98b32accacd475a
bacancy-urmit <urmit.prajapati@bacancytechnology.com>
2020-04-02 14:46:01 +0530

    added provider and uid field in user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_124640) do
+ActiveRecord::Schema.define(version: 2020_04_01_121801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -146,6 +146,8 @@ ActiveRecord::Schema.define(version: 2020_03_30_124640) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "provider"
+    t.string "uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -167,4 +169,3 @@ ActiveRecord::Schema.define(version: 2020_03_30_124640) do
   add_foreign_key "resumes", "users"
   add_foreign_key "subscriptions", "companies"
 end
-
