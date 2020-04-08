8a0e944c9b28c974c7b016392fa55a0552c06de2
bacancy-urmit <urmit.prajapati@bacancytechnology.com>
2020-04-08 09:59:18 +0530

    change in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,9 +13,6 @@ require("channels")
 require("packs/user_index_page.js")
 require("packs/jquery.raty")
 require("packs/main.js")
-require("packs/moment.js")
-require("packs/theme/*")
-// require("packs/main.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
