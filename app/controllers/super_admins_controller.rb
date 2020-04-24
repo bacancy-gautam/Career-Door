0f2eac71905b15b022c532e0c1a39174ad76efb4
vatsalmewada <vatsaljmewada@gmail.com>
2020-04-24 12:44:59 +0530

    ui changes in admin and company

--- app/controllers/super_admins_controller.rb
+++ app/controllers/super_admins_controller.rb
@@ -2,7 +2,13 @@
 
 # super admins controller
 class SuperAdminsController < ApplicationController
+  before_action :authenticate_user!, :check_user
+
   def index
     @companies = Company.where(approved: false)
+  end
+
+  def check_user
+    redirect_to root_path if current_user.has_role?(:user)
   end
 end
